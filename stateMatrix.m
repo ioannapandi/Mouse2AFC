00af92e94d24c1d0ddcf59bf39fe2552f19f9f16
ioanna <ioanpand9@gmail.com>
2020-10-12 13:01:25 +0300

    corrections in rename variable: L/RSoundPort

--- stateMatrix.m
+++ stateMatrix.m
@@ -50,8 +50,8 @@ elseif TaskParameters.GUI.ExperimentType == ExperimentType.SoundDiscrimination
     % Divide maxsound by 100 to get fraction value
     LeftPWMSound = round(BpodSystem.Data.Custom.SoundLeft(iTrial)*LeftSoundPWM/100);
     RightPWMSound = round(BpodSystem.Data.Custom.SoundRight(iTrial)*RightSoundPWM/100);
-    DeliverStimulus = {strcat('PWM',num2str(LeftSound)),LeftPWMSound,...
-                       strcat('PWM',num2str(RightSound)),RightPWMSound};
+    DeliverStimulus = {strcat('PWM',num2str(LeftSoundPort)),LeftPWMSound,...
+                       strcat('PWM',num2str(RightSoundPort)),RightPWMSound};
     ContDeliverStimulus = DeliverStimulus;
     StopStimulus = iff(TaskParameters.GUI.StimAfterPokeOut, DeliverStimulus, {});
     ChoiceStopStimulus = {};
